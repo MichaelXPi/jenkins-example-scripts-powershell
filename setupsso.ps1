271d6018b61c2c84a4f5328170990bba4bd8eafa
Michael Pi <53343391+MichaelXPi@users.noreply.github.com>
2024-01-11 16:40:23 -0600

    Update setupsso.ps1

--- setupsso.ps1
+++ setupsso.ps1
@@ -1,5 +1,6 @@
 #!/usr/bin/pwsh -Command
 param([String] $enterpriseId, [String] $partyCode, [string] $certString)
+
 echo "Setting up SSO for Enterprise $enterpriseId with PartyCode $partyCode..."
 $cert = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($certString))
 echo $cert
