f7df08893a89c57202801ce2d9b8109ce8055bcc
Michael Pi <53343391+MichaelXPi@users.noreply.github.com>
2024-01-11 16:44:03 -0600

    Update setupsso.ps1

--- setupsso.ps1
+++ setupsso.ps1
@@ -3,5 +3,6 @@ param([String] $enterpriseId, [String] $partyCode, [string] $certString)
 
 echo "Setting up SSO for Enterprise $enterpriseId with PartyCode $partyCode..."
 $cert = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($certString))
+$cert.Trim()
 echo $cert
 echo "SSO for Enterprise $enterpriseId has been setup..."
