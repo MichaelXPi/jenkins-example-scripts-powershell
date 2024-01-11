91593987a2664ce6a8e811d845d2d553f7776590
Michael Pi <53343391+MichaelXPi@users.noreply.github.com>
2024-01-11 12:55:09 -0600

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/pwsh -Command
-param([String] $enterpriseId, [String] $partyCode, [string] certString)
+param([String] $enterpriseId, [String] $partyCode, [string] $certString)
 echo "Hello World!"
 echo $enterpriseId
 echo $partyCode
-echo certString
+echo $certString
